9ce02a5bc26b44af60cfc6439acba5cae66dbc1d
Khuram Malik <eemouk@gmail.com>
2011-11-29 20:39:53 +0000

    css/js blank files fix

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,17 @@
 
 */
 
+$(function(){
+
+//Aristo Hover
+				$('#dialog_link, ul#icons li').hover(
+					function() { $(this).addClass('ui-state-hover'); }, 
+					function() { $(this).removeClass('ui-state-hover'); }
+				);
+// Aristo Button
+				$("#divButton, #linkButton, #submitButton, #inputButton").button();	
+});
+
 
 
 
