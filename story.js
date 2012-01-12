d886695ce4b1257c7297286f0e662e0a91ab0eca
Khuram Malik <eemouk@gmail.com>
2012-01-12 21:50:19 +0000

    merge db and story.js

--- story.js
+++ story.js
@@ -1,6 +1,11 @@
+//Storify -- Khuram Malik
+
+// Required Modules
 var app = require('express').createServer()
   , io = require('socket.io').listen(app)
-  , express = require('express');
+  , express = require('express')
+  , mongoose = require ('mongoose')
+  , Schema = mongoose.Schema;
 
 
 
@@ -14,10 +19,55 @@ app.get('/', function (req, res) {
 
 app.use(express.static(__dirname + '/public'));
 
+//Create Schema	
 
+var Lines = new Schema({
+    author  : String,
+	text  : String
+});
+
+var Story = new Schema ({
+	
+	maxlines: {type: Number, default: 3}, // Max number of lines per user
+	date: {type: Date, default: Date.now},
+	title: String,
+	lines: [Lines]
+	
+	});
+
+
+mongoose.connect('mongodb://localhost/test');
+
+//setup model and pass it schema
+mongoose.model ('Story',Story);
+
+var StoryModel = mongoose.model ('Story');
+
+var story = new StoryModel();
+
+// Fixed params
+story.title = 'Last Samurai';
+
+
+//Capture data from socket into schema
 io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   socket.on('slog', function (data) {
-    console.log(data.my);
+  story.lines.push ({ author: 'Khuram', text: data.my}, function (){
+  	
+  		story.save(function(err){
+
+		//save line
+		if (err) {throw err; }
+		console.log('saved story line');
+		});
+
+  	});
+	
   });
 });
+
+
+//disconnect db
+mongoose.disconnect();
+	
